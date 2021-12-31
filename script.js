cacf26b6ba5b1fbc6d651522985fcc0b63c3093d
orellachichester@gmail.com <orellachichester@gmail.com>
2021-12-30 23:31:39 -0500

    Second Commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const worstPerformances = [
+    {
+    question: "Which celebrity had to issue an apology in 2004 after they were caught lip syncing live on SNL?",
+    answers: {
+        a: "Beyonce",
+        b: "Ashlee Simpson",
+        c: "Jessica Simpson",
+    },
+    correctAnswer: "b"
+},
+{
+question: "Which celebrity was the victim of an infamous wardrobe malfunction during the 2004 Super Bowl halftime show?",
+answers: {
+    a: "Janet Jackson",
+    b: "Britney Spears",
+    c: "Madonna"
+},
+correctAnswer: "a"
+}
+];
+
+
+
+
